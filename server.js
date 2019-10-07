18537bd63dde22e2ab6259d857d7e2314012f8b6
Ben <bigriffin94@outlook.com>
2019-10-07 16:08:00 -0700

    SQL - Write Basic Queries - complete. Project Setup, deps, server and routers created.

--- server.js
+++ server.js
@@ -1,9 +1,19 @@
+// Imports
 const express = require('express');
 
-const db = require('./data/dbConfig.js');
+// Need our Router 
+const accountRouter = require('./Routers/accountsRouter.js');
+
+// *** I am using my dbConfig file in my accounts Router file
+// const db = require('./data/dbConfig.js');
 
 const server = express();
 
 server.use(express.json());
 
+// ORDER MATTERS
+    // add our accountRouter to be used by the server
+server.use('api/accounts', accountRouter)
+
+// Always Export the server.
 module.exports = server;
